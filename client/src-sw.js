e09d051483ab32a1d79af878c8201e6bfad4e106
VJacobo <victoriajacobo99@gmail.com>
2024-02-01 16:31:25 -0600

    added to src-sw.js. Implemented asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,17 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+    ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'image',
+    new StaleWhileRecalidate({
+        cacheName: 'assets-cache',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new ExpirationPlugin({
+                maxAgeSeconds: 7 * 24 * 60 * 60, // caches for 7 days
+            }),
+        ],
+    })
+);
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,6 +1,6 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
+const initDb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
@@ -12,25 +12,26 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented') {
-  const db = await initdb();
-  const tx = = db.transaction('jate', 'readwrite');
+// Method to add content to the database
+export const putDb = async (content) => {
+  const db = await initDb();
+  const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store .put({ content });
+  await store.put({ content });
   await tx.done;
-  console.log('Data retrieved from database');
+  console.log('Data added to the database');
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented') {
-  const db = await initdb();
-  const tx = db.transaction('jate', 'readwrite');
+// Method to get all content from the database
+export const getDb = async () => {
+  const db = await initDb();
+  const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const data = await store.getAll();
   await tx.done;
-  console.log('Data retrieved from database');
-  return data.map(item => item.content)[0]
+  console.log('Data retrieved from the database');
+  return data.map(item => item.content);
 };
 
-initdb();
+// Initialize the database when the script runs
+initDb();
